e80fd291a9ca0125d789bac2e9af71f9a4fe14d6
Wallace dos Anjos <wallace.anjos@al.infnet.edu.br>
2020-12-06 20:46:25 -0300

    Add project files.

---
+++ AwesomeTaskList.Infrastructure/Class1.cs
@@ -0,0 +1,22 @@
+﻿using AwesomeTaskList.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace AwesomeTaskList.Infrastructure
+{
+    public class AwesomeTaskListDbContext : DbContext
+    {
+        public DbSet<TaskItem> TaskItems { get; set; }
+
+        public AwesomeTaskListDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AwesomeTaskListDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
